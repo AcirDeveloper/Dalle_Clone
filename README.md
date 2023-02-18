e652863c88970101ff09eb759d327a4f5d92927c
Julio Sevillano <acirdeveloer@gmail.com>
2023-02-17 23:52:51 -0500

    Create post part_1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Dalle_Clone
+# DALL-E CLONE
+
+Folder Client contains the React App Constructed with Vite and TailwindCSS.
+
+- Node: 18.14.0
+- NPM: 9.3.1
+
+Folder Server contains the MongoDB Database and the NodeJS Server.
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,56 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { preview } from '../assets'
+import { getRandomPrompt } from '../utils'
+import { FormField, Loader } from '../components'
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  })
+
+  const [loading, setLoading] = useState(false)
+  const [generatingImg, setGeneratingImg] = useState(false)
+
+  const handleSubmit = () => {}
+  const handleChange = (e) => {}
+  const handleSurpriseMe = () => {}
+
+  return (
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w-[500px]'>
+          Create imaginative and visually stunnning images through by DALL-E AI and share them with
+          the community
+        </p>
+      </div>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName='Your Name'
+            placeholder='Enter your name'
+            type='text'
+            name='name'
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName='Prompt'
+            placeholder='A plush toy robot sitting against a yellow wall'
+            type='text'
+            name='prompt'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  )
 }
 
 export default CreatePost
